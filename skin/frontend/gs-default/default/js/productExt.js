c8fd69e9547811974c6edda387f9f73d010c5f1d
Awesemo <Awesemo@LANPARTY-DK.(none)>
2012-08-01 22:34:08 +0800

    - fixed reloadPrice bug

--- skin/frontend/gs-default/default/js/productExt.js
+++ skin/frontend/gs-default/default/js/productExt.js
@@ -75,7 +75,9 @@ Product.Config.prototype.configureElement = function(element, initLoad) {
         this.resetChildren(element);
     }
     //if (this.settings[this.settings.length-1].attributeId == element.
-    //this.reloadPrice();
+    if (!initLoad) {
+        this.reloadPrice();
+    }
 };
 
 /**
