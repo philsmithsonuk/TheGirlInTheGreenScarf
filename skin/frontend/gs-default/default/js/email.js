4a36d1700e13f17bd85ae0e33e07e55a33766515
Awesemo <Awesemo@LANPARTY-DK.(none)>
2012-09-03 00:32:54 +0800

    - updated

--- skin/frontend/gs-default/default/js/email.js
+++ skin/frontend/gs-default/default/js/email.js
@@ -1,8 +1,7 @@
 jQuery(document).ready(function($){
-    el = $('a.email_address');
-    el.each(function(){
-        el.attr('href','mailto:' + el.attr('href').replace('|','@').replace('/','').replace(':','.'));
-        el.attr('title',el.attr('href').replace('|','@').replace('/','').replace(':','.').replace('mailto.','Email: '));
-        el.html(el.html().replace('|','@').replace('/','').replace(':','.'));
+    $('a.email_address').each(function(itr,el){
+        $(el).attr('href','mailto:' + $(el).attr('href').replace('|','@').replace('/','').replace(':','.'));
+        $(el).attr('title',$(el).attr('href').replace('|','@').replace('/','').replace(':','.').replace('mailto.','Email: '));
+        $(el).html($(el).html().replace('|','@').replace('/','').replace(':','.'));
     });
 });
